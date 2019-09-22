00143722daf56637845027ea1122f51cfccd5f38
wyxpr <45851562+wyxpr@users.noreply.github.com>
2019-09-22 11:34:00 +0800

    Update labinstall.sh

--- labinstall.sh
+++ labinstall.sh
@@ -4,8 +4,12 @@ sudo apt-get install git
 sudo chmod -R 777 /var/www/html
 cd /var/www/html
 git clone https://github.com/wyxpr/labinstall.git
+cd labinstall
+mv $1 /var/www/html
+cd /var/www/html
 unzip $1
 rm -rf $1
+rm -rf labinstall
 clear
 echo 安装完成！
 
